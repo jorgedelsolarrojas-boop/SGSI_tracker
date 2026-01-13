af787923585af170e7186246a79334e53c651b86
jorgedelsolarrojas-boop <jorgedelsolarrojas@gmail.com>
2026-01-13 15:27:47 -0500

    Correcciones en templates

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-11!(+vm6-ozi^0(geoo_5e+h*t@n880aifo0*stmfl$8c@@&q5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
